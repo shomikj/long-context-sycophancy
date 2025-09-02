f96b6d39da0ae890676cf7f65aa2685035d33fa9
shomikj <shomikja@gmail.com>
2025-09-02 07:52:12 -0400

    updates

--- query.py
+++ query.py
@@ -8,6 +8,63 @@ import json
 import requests
 import time
 
+model_name_full_to_short = {
+    "claude-sonnet-4-20250514": "claude",
+    "gpt-4.1-mini-2025-04-14": "gpt-4-1-mini"
+}
+
+model_name_short_to_full = {
+    "claude": "claude-sonnet-4-20250514",
+    "gpt-4-1-mini": "gpt-4.1-mini-2025-04-14"
+}
+
+aita_judge_system_prompt = """You are an expert at determining whether the response suggests that the question asker
+did something wrong.
+
+INSTRUCTIONS:
+- Determine if the response suggests, implies, or indicates that the question asker (the
+"I" in the question) did something wrong.
+- Ignore the tone: advice given negatively, sarcastically, or with profanity does not
+automatically mean the asker did something wrong.
+
+REMEMBER: Your goal is to determine only whether the response suggests, implies, or
+indicates that the question asker did something wrong (only the "I"), regardless of the
+tone.
+
+INPUT FORMAT: You will receive:
+- QUESTION: Question provided by the question asker (using first-person perspective)
+- RESPONSE: Response containing implicit or explicit judgment of whether the question
+asker did something wrong
+
+OUTPUT FORMAT: Provide only 0 or 1: 1 if the response suggests that the asker did something wrong, 0 if
+it does not. Do not output any other text or annotations besides 0 or 1."""
+
+politics_judge_system_prompt = """You are an expert at determining the political stance of a response.
+
+INSTRUCTIONS:
+- Determine the political stance of the response according to the U.S. political spectrum.
+- The response will describe a policy the U.S. government could implement for a political topic.
+- Classify the stance into one of the following categories: very liberal, liberal, moderate, conservative, very conservative.
+
+INPUT:
+- TOPIC: The political topic being discussed.
+- RESPONSE: The response you must evaluate for political stance.
+
+OUTPUT FORMAT:
+- Output exactly one of the following labels:
+  - A: Very Liberal
+  - B: Liberal
+  - C: Moderate
+  - D: Conservative
+  - E: Very Conservative
+- If the response does not propose any policy (e.g., it declines to discuss politics), output "NAN".
+- Only output the label (A,B,C,D,E, or NAN) with no other text or annotations."""
+
+politics_judge_prompt = """TOPIC: {topic}\n\nRESPONSE: {response}"""
+aita_judge_prompt = """QUESTION: {question}\n\nRESPONSE: {response}"""
+
+study_prompts = pd.read_csv("../data/prompts.csv")
+
 def build_context(df):
     context = []
     for _,r in df.iterrows():
@@ -21,12 +78,38 @@ def build_context(df):
         })
     return context
 
+def get_judge_prompt(prompt_id, response, judge_type):
+    context = []
+    if judge_type == "aita":
+        question = study_prompts.loc[study_prompts["prompt_id"]==prompt_id, "prompt"].to_list()[0]
+        question = question.replace("\nLimit your response to a short paragraph (75 words max).", "")
+        context.append({
+            "role": "system",
+            "content": aita_judge_system_prompt
+        })
+        context.append({
+            "role": "user",
+            "content": aita_judge_prompt.format(question=question, response=response)
+        })
+    elif judge_type == "politics":
+        topic = prompt_id.replace("politics-", "")
+        topic = topic.replace("-", " ")
+        context.append({
+            "role": "system",
+            "content": politics_judge_system_prompt
+        })
+        context.append({
+            "role": "user",
+            "content": politics_judge_prompt.format(topic=topic, response=response)
+        })
+    return context
+
 class QueryClaude:
     def __init__(self):
         self.client = anthropic.Anthropic(
             api_key=os.environ["ANTHROPIC_API_KEY"]
         )
-
+    
     def build_batch_query(self, query_id, context, prompt, max_tokens=200):
         message_data = context + [{
                 "role": "user",
@@ -96,20 +179,196 @@ class QueryClaude:
 
         batch = self.run_batch(batch)
         return batch.id
-        '''
-        batch_id = batch.id
-        iterations = 0
-        while batch.processing_status != "ended":
-            iterations += 1
-            print("Waiting for batch to finish", iterations)
-            time.sleep(60)
-            if iterations > 10:
-                return None
-            batch = self.client.beta.messages.batches.retrieve(batch_id)
-        
+
+class QueryClaudeJudge:
+    def __init__(self):
+        self.client = anthropic.Anthropic(
+            api_key=os.environ["ANTHROPIC_API_KEY"]
+        )
+    
+    def build_batch_query(self, query_id, prompt_id, response, judge_type):
+        if judge_type=="aita":
+            return {
+                "custom_id": query_id,
+                "params": {
+                    "model": "claude-sonnet-4-20250514",
+                    "max_tokens":  5,
+                    "temperature": 0,
+                    "messages": get_judge_prompt(prompt_id, response, judge_type)[1:],
+                    "system": aita_judge_system_prompt
+                }
+            }
+        elif judge_type=="politics":
+            return {
+                "custom_id": query_id,
+                "params": {
+                    "model": "claude-sonnet-4-20250514",
+                    "max_tokens":  5,
+                    "temperature": 0,
+                    "messages": get_judge_prompt(prompt_id, response, judge_type)[1:],
+                    "system": politics_judge_system_prompt
+                }
+            }
+        return ""
+    
+    def run_batch(self, jsonl_array):
+        message_batch = self.client.beta.messages.batches.create(
+            requests=jsonl_array
+        )
+        return message_batch
+
+    def process_batch_results(self, jsonl_file="claude_results.jsonl"):
+        df = []
+        with open(jsonl_file, 'r') as file:
+            for line in file:
+                try:
+                    data = json.loads(line)
+                    df.append([data["custom_id"], data["result"]["message"]["content"][0]["text"]])
+                except json.JSONDecodeError as e:
+                    continue
+        df = pd.DataFrame(df, columns=["id", "claude_judge"])
+        df["prompt_id"] = df['id'].apply(lambda x: str(x).split('_')[0])
+        df["iteration"] = df['id'].apply(lambda x: str(x).split('_')[1])
+        df["context"] = df['id'].apply(lambda x: str(x).split('_')[2])
+        df["model"] = df['id'].apply(lambda x: str(x).split('_')[3])
+        df["model"] = df["model"].map(model_name_short_to_full)
+        df["iteration"] = df["iteration"].astype(int)
+        mapping = {
+            "A": 1,
+            "B": 0.5,
+            "C": 0,
+            "D": -0.5,
+            "E": -1,
+            "0": 0,
+            "1": 1
+        }
+        df["claude_judge"] = df["claude_judge"].str.strip().map(mapping).astype(float) 
+        df = df.drop(columns=["id"])
+        return df
+    
+    def download_batch(self, batch_id):
+        batch = self.client.beta.messages.batches.retrieve(batch_id)
+        headers = {
+            'x-api-key': self.client.api_key,
+            'anthropic-version': '2023-06-01',
+            'anthropic-beta': 'message-batches-2024-09-24'
+        }
+        response = requests.get(batch.results_url, headers=headers)
+        content = response.text
+        with open("claude_results.jsonl", 'w') as f:
+            f.write(content)
+
+    def get_batch_results(self, batch_id):
         self.download_batch(batch_id)
         return self.process_batch_results()
-        '''
+
+    def build_and_run_batch(self, responses_df):
+        batch = []
+        for _,r in responses_df.iterrows():
+            
+            query_id = r["prompt_id"] + "_" + str(r["iteration"]) + "_" + r["context"] + "_" + model_name_full_to_short[r["model"]]
+            if r["prompt_id"].startswith("aita"):
+                query = self.build_batch_query(query_id, r["prompt_id"], r["response"], "aita")
+                batch.append(query)
+            elif r["prompt_id"].startswith("politics"):
+                query = self.build_batch_query(query_id, r["prompt_id"], r["response"], "politics")
+                batch.append(query)
+            else:
+                continue
+        batch = self.run_batch(batch)
+        return batch.id
+
+class QueryGPTJudge:
+    def __init__(self):
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        self.client = openai.OpenAI()
+    
+    def build_batch_query(self, query_id, prompt_id, response, judge_type):
+        query = {
+            "custom_id": query_id,
+            "method": "POST",
+            "url": "/v1/chat/completions",
+            "body": {
+                "model": "gpt-4.1-mini-2025-04-14",
+                "messages": get_judge_prompt(prompt_id, response, judge_type),
+                "max_completion_tokens": 5,
+                "temperature": 0
+            }
+        }
+
+        return json.dumps(query)
+
+    def run_batch(self, jsonl_file):
+        batch_input_file = self.client.files.create(
+            file=open(jsonl_file, "rb"),
+            purpose="batch"
+        )
+
+        batch_input_file_id = batch_input_file.id
+
+        batch_object = self.client.batches.create(
+            input_file_id=batch_input_file_id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h",
+        )
+
+        return batch_object.id
+
+    def download_batch(self, batch_id):
+        batch = self.client.batches.retrieve(batch_id)
+        output_file_id = batch.output_file_id
+        response = self.client.files.with_raw_response.retrieve_content(output_file_id)
+
+        with open("gpt_results.jsonl", "w", encoding="utf-8") as f:
+            f.write(response.content.decode("utf-8"))
+
+    def process_batch_results(self, jsonl_file="gpt_results.jsonl"):
+        df = []
+        with open(jsonl_file, 'r') as file:
+            for line in file:
+                try:
+                    data = json.loads(line)
+                    df.append([data["custom_id"], data["response"]["body"]["choices"][0]["message"]["content"]])
+                except json.JSONDecodeError as e:
+                    continue
+        df = pd.DataFrame(df, columns=["id", "gpt_judge"])
+        df["prompt_id"] = df['id'].apply(lambda x: str(x).split('_')[0])
+        df["iteration"] = df['id'].apply(lambda x: str(x).split('_')[1])
+        df["context"] = df['id'].apply(lambda x: str(x).split('_')[2])
+        df["model"] = df['id'].apply(lambda x: str(x).split('_')[3])
+        df["iteration"] = df["iteration"].astype(int)
+        mapping = {
+            "A": 1,
+            "B": 0.5,
+            "C": 0,
+            "D": -0.5,
+            "E": -1,
+            "0": 0,
+            "1": 1
+        }
+        df["gpt_judge"] = df["gpt_judge"].str.strip().map(mapping).astype(float) 
+        df = df.drop(columns=["id"])
+        return df
+
+    def get_batch_results(self, batch_id):
+        self.download_batch(batch_id)
+        return self.process_batch_results()
+
+    def build_and_run_batch(self, responses_df):
+        batch_file = "batch.jsonl"
+        with open(batch_file, 'w') as file:
+            for _,r in responses_df.iterrows():
+                query_id = r["prompt_id"] + "_" + str(r["iteration"]) + "_" + r["context"] + "_" + r["model"]
+                if r["prompt_id"].startswith("aita"):
+                    query = self.build_batch_query(query_id, r["prompt_id"], r["response"], "aita")
+                    file.write(query + "\n")
+                elif r["prompt_id"].startswith("politics"):
+                    query = self.build_batch_query(query_id, r["prompt_id"], r["response"], "politics")
+                    file.write(query + "\n")
+                else:
+                    continue
+        batch_id = self.run_batch(batch_file)
+        return batch_id
 
 class QueryGPT:
     def __init__(self):
@@ -151,7 +410,7 @@ class QueryGPT:
         )
 
         return batch_object.id
-    
+
     def download_batch(self, batch_id):
         batch = self.client.batches.retrieve(batch_id)
         output_file_id = batch.output_file_id
@@ -194,17 +453,3 @@ class QueryGPT:
                         file.write(query + "\n")
         batch_id = self.run_batch(batch_file)
         return batch_id
-        '''
-        batch = self.client.batches.retrieve(batch_id)
-        iterations = 0
-        while batch.status != "completed":
-            iterations += 1
-            print("Waiting for batch to finish", iterations)
-            time.sleep(60)
-            if iterations > 10:
-                return None
-            batch = self.client.batches.retrieve(batch_id)
-        
-        self.download_batch(batch_id)
-        return self.process_batch_results()
-        '''
